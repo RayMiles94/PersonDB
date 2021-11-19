818eda9e98129b773158e183c6b017c3ce19bc37
RayMiles94 <natsuboy94@gmail.com>
2021-11-19 10:08:00 +0100

    [PERSONDB] try to work with handlerbars

--- src/main.ts
+++ src/main.ts
@@ -3,13 +3,16 @@ import { NestExpressApplication } from '@nestjs/platform-express';
 import { join } from 'path';
 import { AppModule } from './app.module';
 import * as hbs from 'hbs';
+// let layout = require('handlebars-layouts');
+// let handlerbars = require('handlebars');
+
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
-  
+  app.setViewEngine('hbs');
   app.useStaticAssets(join(__dirname, '..', 'public'));
   app.setBaseViewsDir(join(__dirname, '..', 'views'));
-  app.setViewEngine('hbs');
+  //  hbs.registerHelper(layout(handlerbars));
   hbs.registerPartials(join(__dirname, '..', '/views/partials'));
   await app.listen(4500);
 }
